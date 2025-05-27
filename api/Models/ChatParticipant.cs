f6220844851598e545aeb04481374de08c51b344
Mostafijur Rahman <mostafijur1812@gmail.com>
2025-05-27 23:48:39 +0600

    Chat participant model ready

---
+++ api/Models/ChatParticipant.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class ChatParticipant
+    {
+        public int Id { get; set; }
+        public int ChatId { get; set; }
+        public int UserId { get; set; }
+        public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
+        public bool IsAdmin { get; set; }
+
+        // Navigation proparties
+        [ForeignKey("ChatId")]
+        public Chat? Chat { get; set; }
+        
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+    }
+}
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ab5f479097db1d3b823737c5358326fa4beac3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ecd5dbd2217f5fbc5eff66ac1aff956f47e3822")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
